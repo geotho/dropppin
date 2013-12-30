4243d45fffa10ebf393efbeb49c85b4b4ad50332
George Thomas <georgetamayothomas@gmail.com>
2013-12-30 00:42:36 +0000

    Add toggle_done, clear_all and clear_done controls

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,28 +1,41 @@
 class PlacesController < ApplicationController
   respond_to :json
 
-  def new
-    @place = Post.new
-  end
-
   def create
-    @place = Place.new(params[:place].permit(:name, :lat, :lng, :reference))
-    if @place.save!
-      render json: @place
-    else
-      render :text => "FUCK"
+    @place = current_user.places
+        .build(params[:place].permit(:name, :lat, :lng, :reference, :address))
+    @place.done = false
+    @place.cleared = false
+    respond_to do |format|
+      if @place.save!
+        format.html { render :partial => "place", :locals => {:place => @place} }
+      else
+        format.json { render json: @place.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def update
-    @place = Place.find(params[:id])
-   
+  def toggle_done
+    @place = current_user.places.find(params[:id])
+
     @place.done = !@place.done
 
-    @place.save()
+    if @place.save!
+      head :no_content
+    else
+      render status: :internal_server_error
+    end
   end
 
-  def index
-    @places = Place.all
+  def clear_done
+    @places = current_user.places.where(done: true).update_all(cleared: true)
+
+    head :no_content
+  end
+
+  def clear_all
+    @places = current_user.places.update_all(cleared: true)
+
+    head :no_content
   end
 end
